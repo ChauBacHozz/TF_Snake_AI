e6edebe959150ac08fa436717e8869c0d94ac030
ChauBacHozz <phongndsaiyan@gmail.com>
2023-06-02 22:57:43 +0700

    add moving mechanism

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import pygame as pg
+import random
 pg.init()
 
 
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
+VELOCITY = 300
 nrows = 20
 ncols = 20 
 grid_val = DISPLAY_HEIGHT / nrows
+
+class Snake():
+    def __init__(self, width):
+        self.nodes = 1
+        self.color = (0, 255, 0)
+        self.width = width
+        self.velocity = 200
+        self.direction = "R"
+    def spawn(self):
+        self.xpos = random.randint(1, 10) * 5
+        self.ypos = random.randint(1, 10) * 5
+    def drawHead(self):
+        pg.draw.rect(gameDisplay ,rect = pg.Rect(round(self.xpos / self.width) * self.width
+                                                 ,round(self.ypos / self.width) * self.width, self.width, self.width), color=self.color)
+    def move(self, dt):
+        if self.direction == "U":
+            self.ypos -= self.velocity * dt
+        if self.direction == "D":
+            self.ypos += self.velocity * dt
+        if self.direction == "L":
+            self.xpos -= self.velocity * dt
+        if self.direction == "R":
+            self.xpos += self.velocity * dt
+    def turn(self, type):
+        if type == "U" and self.direction != "D":
+            self.direction = "U"
+        if type == "D" and self.direction != "U":
+            self.direction = "D"
+        if type == "L" and self.direction != "R":
+            self.direction = "L"
+        if type == "R" and self.direction != "L":
+            self.direction = "R"
 def drawGrid():
     for i in range(nrows + 1):
         pg.draw.line(gameDisplay, (255, 255, 255, 255),
@@ -24,19 +58,29 @@ running = True
 xpos = 0
 ypos = 0
 
-
+snake = Snake(grid_val)
+snake.spawn()
 while running:
+    dt = clock.tick(60) / 1000
     # Quit button
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    xpos += 1
-    ypos += 1
-    print(xpos)
+
+    # Check pressed key
+    keys = pg.key.get_pressed()
+    if keys[pg.K_UP]:
+        snake.turn("U")
+    if keys[pg.K_DOWN]:
+        snake.turn("D")
+    if keys[pg.K_LEFT]:
+        snake.turn("L")
+    if keys[pg.K_RIGHT]:
+        snake.turn("R")
     gameDisplay.fill(back)
     drawGrid()
-    # pg.draw.rect(gameDisplay ,rect = pg.Rect(xpos, ypos, 100, 100), color=(255, 0, 0))
-
+    snake.move(dt)
+    snake.drawHead()
 
 
 
@@ -44,7 +88,6 @@ while running:
 
     # pg.display.update()
     pg.display.flip()
-    clock.tick(60)   
 
 pg.quit()
 quit()
